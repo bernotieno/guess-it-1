b8247b962e0901dc6e5996106868fee3723e5d3b
bernaotieno <bernadokumu1@gmail.com>
2024-07-23 14:09:19 +0300

    update: used newscanner for its efficiency

--- student/inputhandler/handler.go
+++ student/inputhandler/handler.go
@@ -5,33 +5,29 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 
 	calc "guess-it-1/calculation"
 )
 
 func HandleInput() {
 	var data []float64
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-
-		if input == "" {
-			fmt.Println("Exiting...")
-			break
-		}
-
-		num, err := strconv.Atoi(input)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		num, err := strconv.ParseFloat(input, 64)
 		if err != nil {
 			fmt.Println("Invalid input. Please enter a valid number.")
 			continue
 		}
 
-		data = append(data, float64(num))
+		data = append(data, num)
 		if len(data) > 1 {
 			lower, upper := calc.Range(data)
 			fmt.Printf("%d %d\n", lower, upper)
 		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
 	}
 }
