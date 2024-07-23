125f127231d2776769f0bd6c15bf046530643971
bernaotieno <bernadokumu1@gmail.com>
2024-07-23 10:04:09 +0300

    used new scanner to scan our input rather than reader

--- student/main.go
+++ student/main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 
-	handler "guess-it-1/inputhandler"
+	calc "guess-it-1/calculation"
 )
 
 func main() {
 	if len(os.Args) != 1 {
-		fmt.Println("Usage: go run main.go ")
+		fmt.Println("Usage: go run main.go")
 		return
 	}
-	handler.HandleInput()
+
+	var data []float64
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		num, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter a valid number.")
+			continue
+		}
+
+		data = append(data, num)
+		if len(data) > 1 {
+			lower, upper := calc.Range(data)
+			fmt.Printf("%d %d\n", lower, upper)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
+	}
 }
