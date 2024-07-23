2a77e6e0a816e80676d391fcb0d04f4b6f7431c0
bernaotieno <bernadokumu1@gmail.com>
2024-07-23 14:07:09 +0300

    update the range to use in banch of 4 to do the calculation

--- student/calculation/range.go
+++ student/calculation/range.go
@@ -3,12 +3,17 @@ package calc
 import "math"
 
 func Range(data []float64) (int, int) {
-	mean := average(data)
-	variance := variance(data)
+	start := len(data) - 4
+	if start < 0 {
+		start = 0
+	}
+	window := data[start:]
+	mean := average(window)
+	variance := variance(window)
 	stddev := standarddev(variance)
 
-	lower := mean - 1.5*stddev
-	upper := mean + 1.5*stddev
+	lower := mean - (1.8 * stddev)
+	upper := mean + (1.8 * stddev)
 
 	return int(math.Round(lower)), int(math.Round(upper))
 }
