9faccf6a32d3f4fbf90ae8e52381c1ab3bcffcbb
bernaotieno <bernadokumu1@gmail.com>
2024-07-23 14:09:55 +0300

    update: main to now call the handler function to be executed

--- student/main.go
+++ student/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 
-	calc "guess-it-1/calculation"
+	handler "guess-it-1/inputhandler"
 )
 
 func main() {
@@ -15,24 +13,5 @@ func main() {
 		return
 	}
 
-	var data []float64
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		input := scanner.Text()
-		num, err := strconv.ParseFloat(input, 64)
-		if err != nil {
-			fmt.Println("Invalid input. Please enter a valid number.")
-			continue
-		}
-
-		data = append(data, num)
-		if len(data) > 1 {
-			lower, upper := calc.Range(data)
-			fmt.Printf("%d %d\n", lower, upper)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading input:", err)
-	}
+	handler.HandleInput()
 }
